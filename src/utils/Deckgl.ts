fcfeba00702f73decd12abd8923451c59b832a2b
Zed(Zijun) Chen <c.zijunchen@gmail.com>
2021-09-25 14:28:26 +0800

    Implement redrawArea (#9)
    * Implement redrawArea.
    
    * remvoe unused file.

--- src/utils/Deckgl.ts
+++ src/utils/Deckgl.ts
@@ -33,11 +33,18 @@ export class TileCanvas {
   }
 }
 
-class Bbox {
+export class Bbox {
   west: number;
   south: number;
   east: number;
   north: number;
+
+  constructor(west: number, south: number, east: number, north: number) {
+    this.west = west;
+    this.south = south;
+    this.east = east;
+    this.north = north;
+  }
 }
 
 export class Tile {
