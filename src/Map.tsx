00682982304964afad5a6da7b0de28503cff38f9
Zed(Zijun) Chen <c.zijunchen@gmail.com>
2021-09-27 08:38:00 -0700

    _

--- src/Map.tsx
+++ src/Map.tsx
@@ -6,23 +6,6 @@ import { MapRenderer } from "./utils/MapRenderer";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN || "";
 
-const iconEraserSolid = (
-  <svg
-    aria-hidden="true"
-    focusable="false"
-    data-prefix="fas"
-    data-icon="eraser"
-    role="img"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 512 512"
-  >
-    <path
-      fill="currentColor"
-      d="M497.941 273.941c18.745-18.745 18.745-49.137 0-67.882l-160-160c-18.745-18.745-49.136-18.746-67.883 0l-256 256c-18.745 18.745-18.745 49.137 0 67.882l96 96A48.004 48.004 0 0 0 144 480h356c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12H355.883l142.058-142.059zm-302.627-62.627l137.373 137.373L265.373 416H150.628l-80-80 124.686-124.686z"
-    ></path>
-  </svg>
-);
-
 function Map(): JSX.Element {
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const deckglContainer = useRef<HTMLCanvasElement | null>(null);
@@ -31,7 +14,7 @@ function Map(): JSX.Element {
   const [eraserMode, setEraserMode] = useState(false);
 
   useEffect(() => {
-    return;
+    mapRenderer.setEraserMod(eraserMode);
   }, [eraserMode]);
 
   useEffect(() => {
@@ -79,3 +62,20 @@ function Map(): JSX.Element {
 }
 
 export default Map;
+
+const iconEraserSolid = (
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fas"
+    data-icon="eraser"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+  >
+    <path
+      fill="currentColor"
+      d="M497.941 273.941c18.745-18.745 18.745-49.137 0-67.882l-160-160c-18.745-18.745-49.136-18.746-67.883 0l-256 256c-18.745 18.745-18.745 49.137 0 67.882l96 96A48.004 48.004 0 0 0 144 480h356c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12H355.883l142.058-142.059zm-302.627-62.627l137.373 137.373L265.373 416H150.628l-80-80 124.686-124.686z"
+    ></path>
+  </svg>
+);
