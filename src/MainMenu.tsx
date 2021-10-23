9ae8e6c1e5c151c8d87c07e7ddbc26d547f5794a
Zed(Zijun) Chen <zed@zijun.dev>
2021-10-23 05:37:40 -0700

    _

--- src/MainMenu.tsx
+++ src/MainMenu.tsx
@@ -69,7 +69,7 @@ export default function MainMenu(props: Props): JSX.Element {
                         <a
                           key={item.name}
                           onClick={() => props.onAction(item.action)}
-                          className="flex items-center p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                          className="flex items-center cursor-pointer p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
                         >
                           <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 text-white sm:h-12 sm:w-12">
                             <item.icon aria-hidden="true" />
