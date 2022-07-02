3d3367c38025df03830aace1a057fe06ee6cdc74
Zed(Zijun) Chen <zed@zijun.dev>
2022-07-03 00:15:59 +0800

    [time-machine]Implement github sso (#46)
    * _
    
    * rename
    
    * implement /api/v1/user/sso
    
    * _

--- server/entity/src/user.rs
+++ server/entity/src/user.rs
@@ -4,10 +4,10 @@ use serde::{Deserialize, Serialize};
 #[derive(Copy, Clone, Serialize, Deserialize, Debug, PartialEq, EnumIter, DeriveActiveEnum)]
 #[sea_orm(rs_type = "String", db_type = "Text")]
 pub enum Language {
-    #[serde(alias = "zh-cn")]
+    #[serde(rename = "zh-cn")]
     #[sea_orm(string_value = "zh-cn")]
     ZhCn,
-    #[serde(alias = "en-us")]
+    #[serde(rename = "en-us")]
     #[sea_orm(string_value = "en-us")]
     EnUs,
 }
@@ -16,15 +16,16 @@ pub enum Language {
 #[sea_orm(table_name = "users")]
 pub struct Model {
     #[sea_orm(primary_key)]
-    pub id: i32,
-    #[sea_orm(column_type = "Text", unique, index)]
-    pub email: String,
+    pub id: i64,
+    #[sea_orm(column_type = "Text", unique, nullable, index)]
+    pub email: Option<String>,
     #[sea_orm(column_type = "Text", nullable)]
     pub password: Option<String>,
+    #[sea_orm(column_type = "Text")]
+    pub contact_email: String,
     #[sea_orm(nullable, unique, index)]
-    pub github_sso_uid: Option<i32>,
-    #[sea_orm(nullable, column_type = "Text")]
-    pub language: Option<Language>,
+    pub github_uid: Option<i64>,
+    pub language: Language,
     pub created_at: DateTimeUtc,
     pub updated_at: DateTimeUtc,
 }
