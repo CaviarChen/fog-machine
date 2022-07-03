356ae4249f2d3259dae51b042f9cd6d90fcc3f01
Zed(Zijun) Chen <zed@zijun.dev>
2022-07-03 22:08:48 +0800

    _

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  env: {
+    node: true,
+  },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    camelcase: ["error", { properties: "always" }],
+  },
+};
