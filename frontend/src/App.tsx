95367108ecd58b9738bdb503acd2328f4a9b9998
Zed(Zijun) Chen <zed@zijun.dev>
2022-07-13 11:39:35 +0800

    home page wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,59 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import { Container, Panel, Content, Divider, Stack } from "rsuite";
+import EditIcon from "@rsuite/icons/Edit";
+import HistoryIcon from '@rsuite/icons/History';
+import { IconProps } from "@rsuite/icons/lib/Icon";
 import "./App.css";
+
+function Item(title: string, Icon: React.FC<IconProps>, description: string) {
+  const [hover, setHover] = useState(false);
+  return (
+    <Panel
+      shaded={hover}
+      bordered
+      bodyFill
+      className="main-item"
+      style={{ display: "inline-block", width: "100%", cursor: "pointer", marginTop: "2vh" }}
+      onClick={() => console.log("AAA")}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <Stack spacing={6}>
+        <Icon style={{ fontSize: "6em", margin: "20px" }} />
+        <Panel header={title}>
+          <p>
+            <small>{description}</small>
+          </p>
+        </Panel>
+      </Stack>
+    </Panel>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <div className="main-body">
+          <div className="main-title">
+            <h1>Fog Machine</h1>
+            <h4>A 3rd extension tool for the app Fog of World</h4>
+          </div>
+
+          <Divider />
+          {Item(
+            "Editor",
+            EditIcon,
+            "A tool for visualizing and editing the data of Fog of World App."
+          )}
+          {Item(
+            "Time Machine",
+            HistoryIcon,
+            "A service for backup and preserve history of the data of Fog of World App."
+          )}
+        </div>
+      </Content>
+    </Container>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "rsuite/dist/rsuite.min.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
