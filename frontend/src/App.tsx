4214bc83fe8ead8381ee9cfe092437b8c4a6eb4d
Zed(Zijun) Chen <zed@zijun.dev>
2022-07-13 14:37:35 +0800

    react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,82 +1,19 @@
 import React, { useState } from "react";
-import { Container, Panel, Content, Divider, Stack } from "rsuite";
-import EditIcon from "@rsuite/icons/Edit";
-import HistoryIcon from "@rsuite/icons/History";
-import { IconProps } from "@rsuite/icons/lib/Icon";
+import { Routes, Route, Link } from "react-router-dom";
 import GithubCorner from "./GithubCorner";
-import "./App.css";
+import Home from "./Home";
 
-// TODO: use `react-router`
-
-function Item(
-  title: string,
-  Icon: React.FC<IconProps>,
-  description: string,
-  onClick: () => void
-) {
-  const [hover, setHover] = useState(false);
-  return (
-    <Panel
-      shaded={hover}
-      bordered
-      bodyFill
-      className="main-item"
-      style={{
-        display: "inline-block",
-        width: "100%",
-        cursor: "pointer",
-        marginTop: "2vh",
-      }}
-      onClick={onClick}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
-      <Stack spacing={6}>
-        <Icon style={{ fontSize: "6em", margin: "20px" }} />
-        <Panel header={title}>
-          <p>
-            <small>{description}</small>
-          </p>
-        </Panel>
-      </Stack>
-    </Panel>
-  );
-}
 
 function App() {
   return (
     <>
       <GithubCorner />
-      <Container>
-        <Content>
-          <div className="main-body">
-            <div className="main-title">
-              <h1>Fog Machine</h1>
-              <h4>A 3rd party extension tool for the app Fog of World</h4>
-            </div>
-
-            <Divider />
-            {Item(
-              "Editor",
-              EditIcon,
-              "A tool for visualizing and editing the data of Fog of World App.",
-              () => {
-                location.href = "/editor";
-              }
-            )}
-            {Item(
-              "Time Machine",
-              HistoryIcon,
-              "A service for backup and preserve history of the data of Fog of World App.",
-              () => {
-                console.log("A");
-              }
-            )}
-          </div>
-        </Content>
-      </Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
-  );
+  )
+
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
