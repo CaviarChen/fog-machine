8789827d3ba6af25200c7fe12c8878e2d2fd022f
Zed(Zijun) Chen <zed@zijun.dev>
2022-09-25 11:53:46 +0800

    _

---
+++ build-frontend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "REACT_APP_MAPBOX_TOKEN:" $REACT_APP_MAPBOX_TOKEN
+echo "CDN_URL:" $CDN_URL
+echo "API_URL:" $API_URL
+echo "----------"
+mkdir -p build/frontend/
+rm -rf build/frontend/*
+(cd frontend && export PUBLIC_URL=$CDN_URL && export REACT_APP_BACKEND_URL=$API_URL && yarn build)
+(cd editor && export PUBLIC_URL="${CDN_URL}editor/" && export REACT_APP_MAPBOX_TOKEN=$REACT_APP_MAPBOX_TOKEN && export REACT_APP_BACKEND_URL=$API_URL && yarn build)
+cp -r frontend/build/ build/frontend/
+mkdir build/frontend/editor/
+cp -r editor/build/ build/frontend/editor/
