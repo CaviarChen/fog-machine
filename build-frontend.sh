4fab6835af08fd16c6ae42127daeb2bfe7b3b8d8
Zed(Zijun) Chen <zed@zijun.dev>
2022-09-25 12:39:18 +0800

    _

--- build-frontend.sh
+++ build-frontend.sh
@@ -7,6 +7,6 @@ mkdir -p build/frontend/
 rm -rf build/frontend/*
 (cd frontend && yarn install && export PUBLIC_URL=$CDN_URL && export REACT_APP_BACKEND_URL=$API_URL && yarn build)
 (cd editor && yarn install && export PUBLIC_URL="${CDN_URL}editor/" && export REACT_APP_MAPBOX_TOKEN=$REACT_APP_MAPBOX_TOKEN && export REACT_APP_BACKEND_URL=$API_URL && yarn build)
-cp -r frontend/build/ build/frontend/
+cp -r frontend/build/* build/frontend/
 mkdir build/frontend/editor/
-cp -r editor/build/ build/frontend/editor/
+cp -r editor/build/* build/frontend/editor/
