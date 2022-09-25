211ac2fad4a41a6ba182123b2d9b9045b3020e48
Zed(Zijun) Chen <zed@zijun.dev>
2022-09-25 15:09:50 +0800

    _

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "rsuite/dist/rsuite.min.css";
+import "./I18n";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
