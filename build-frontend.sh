ee3436c6478e610b31df9e7ccd8ef17f55e11c34
Licheng Mao <tavimori@gmail.com>
2022-10-16 17:56:43 +0000

    fix ci dir misconfig

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 yarn install && yarn cicheck && export PUBLIC_URL=$CDN_URL && export REACT_APP_BACKEND_URL=$API_URL && yarn build
-# mkdir -p build/frontend/
+mv build build2
+mkdir -p build/frontend/
+cp -r build2/* build/frontend/
 # rm -rf build/frontend/*
 # (cd frontend && yarn install && yarn cicheck && export PUBLIC_URL=$CDN_URL && export REACT_APP_BACKEND_URL=$API_URL && yarn build)
 # (cd editor && yarn install && yarn cicheck && export PUBLIC_URL="${CDN_URL}editor/" && export REACT_APP_MAPBOX_TOKEN=$REACT_APP_MAPBOX_TOKEN && export REACT_APP_BACKEND_URL=$API_URL && yarn build)
