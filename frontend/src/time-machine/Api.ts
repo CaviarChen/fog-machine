CaviarChen/fog-machine

commit 11a044c23ec643455d3cbe2e5a0eb83280628097

Snapshot list pagination (#71)
1. update snapshots pagination
2. fix api [snakeToCamel] and [camelToSnake]

Co-authored-by: Zed(Zijun) Chen <zed@zijun.dev>

frontend/src/time-machine/Api.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import axios from "axios";
-import lodash from "lodash";
+import { toCamel, toSnake } from "snake-camel";
 
 // I really want ADT x2
 type Ok<T> = {
@@ -52,25 +52,17 @@ export type Snapshot = {
   note: string | null;
 };
 
+export type SnapshotList = {
+  numberOfSnapshots: number;
+  numberOfPages: number;
+  snapshots: Snapshot[];
+};
+
 export type SnapshotUploadResult = {
   id: number;
   fileCount: number;
   logs: string;
 };
-
-// TODO: [snakeToCamel] and [camelToSnake] are very silly.
-function snakeToCamel(x: any): any {
-  lodash.map;
-  return lodash.mapKeys(x, function (_value, key) {
-    return lodash.camelCase(key);
-  });
-}
-
-function camelToSnake(x: any): any {
-  return lodash.mapKeys(x, function (_value, key) {
-    return lodash.snakeCase(key);
-  });
-}
 
 export default class Api {
   public static readonly backendUrl =
@@ -148,7 +140,7 @@ export default class Api {
       code,
     });
     if (result.ok) {
-      result.ok = snakeToCamel(result.ok);
+      result.ok = toCamel(result.ok);
       const data: GithubSsoResponse = result.ok;
 
       if (data.login && data.token) {
@@ -168,7 +160,7 @@ export default class Api {
       "user/sso",
       "post",
       false,
-      camelToSnake({
+      toSnake({
         registrationToken,
         contactEmail,
         language,
@@ -187,11 +179,9 @@ export default class Api {
   > {
     let result = await this.requestApi("snapshot_task", "get", true);
     if (result.ok) {
-      result.ok = snakeToCamel(result.ok);
+      result.ok = toCamel(result.ok);
       if (result.ok.source["OneDrive"]) {
-        result.ok.source["OneDrive"] = snakeToCamel(
-          result.ok.source["OneDrive"]
-        );
+        result.ok.source["OneDrive"] = toCamel(result.ok.source["OneDrive"]);
       }
     } else if (result.status == 404) {
       result = { ok: "none", status: 404 };
@@ -213,7 +203,7 @@ export default class Api {
     }
     if (oneDriveShareUrl) {
       data["source"] = {
-        OneDrive: camelToSnake({
+        OneDrive: toSnake({
           shareUrl: oneDriveShareUrl,
         }),
       };
@@ -247,7 +237,7 @@ export default class Api {
     data["upload_token"] = uploadToken;
     const result = await this.requestApi("snapshot", "post", true, data);
     if (result.ok) {
-      result.ok = snakeToCamel(result.ok);
+      result.ok = toCamel(result.ok);
     }
     return result;
   }
@@ -259,7 +249,7 @@ export default class Api {
     const data: any = {};
     data["interval"] = interval;
     data["source"] = {
-      OneDrive: camelToSnake({
+      OneDrive: toSnake({
         shareUrl: oneDriveShareUrl,
       }),
     };
@@ -279,10 +269,17 @@ export default class Api {
     return result;
   }
 
-  public static async listSnapshots(): Promise<Result<Snapshot[]>> {
-    const result = await this.requestApi("snapshot", "get", true);
+  public static async listSnapshots(
+    page: number,
+    perPage: number
+  ): Promise<Result<SnapshotList>> {
+    const result = await this.requestApi(
+      "snapshot?page=" + String(page) + "&page_size=" + String(perPage),
+      "get",
+      true
+    );
     if (result.ok) {
-      result.ok = result.ok.map((x: any) => snakeToCamel(x));
+      result.ok = toCamel(result.ok);
     }
     return result;
   }

frontend/src/time-machine/DashboardSnapshot.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import moment from "moment";
 import {
   Button,
@@ -9,6 +9,7 @@ import {
   Tooltip,
   Whisper,
   Tag,
+  Pagination,
   Notification,
   useToaster,
   Modal,
@@ -20,7 +21,7 @@ import {
   IconButton,
 } from "rsuite";
 import MoreIcon from "@rsuite/icons/legacy/More";
-import Api, { Snapshot } from "./Api";
+import Api, { SnapshotList, Snapshot } from "./Api";
 import PlusIcon from "@rsuite/icons/Plus";
 import { MessageType } from "rsuite/esm/Notification/Notification";
 import { useTranslation } from "react-i18next";
@@ -29,19 +30,32 @@ const { Column, HeaderCell, Cell } = Table;
 
 function DashboardSnapshot() {
   const { t } = useTranslation();
-  const [snapshots, setSnapshots] = useState<Snapshot[] | null>(null);
-  const loadData = async () => {
-    const result = await Api.listSnapshots();
+  type SnapshotListState = {
+    currentPage: number;
+  };
+  const [snapshotList, setSnapshotList] = useState<SnapshotList | null>(null);
+  const [snapshotListState, setSnapshotListState] = useState<SnapshotListState>(
+    {
+      currentPage: 1,
+    }
+  );
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadData = useCallback(async () => {
+    setIsLoading(true);
+    const result = await Api.listSnapshots(snapshotListState.currentPage, 10);
     if (result.ok) {
-      setSnapshots(result.ok);
+      setSnapshotList(result.ok);
     } else {
       console.log(result);
     }
-  };
+    setIsLoading(false);
+  }, [snapshotListState]);
 
   useEffect(() => {
     loadData();
-  }, []);
+  }, [loadData]);
 
   // TODO: we should have a single global toaster for all notifications
   const notificationToaster = useToaster();
@@ -109,101 +123,128 @@ function DashboardSnapshot() {
   };
 
   const Detail = () => {
-    if (!snapshots) {
+    if (!snapshotList) {
       return <Placeholder.Paragraph />;
     } else {
       return (
-        <Table data={snapshots} id="table">
-          <Column flexGrow={10}>
-            <HeaderCell>{t("snapshot-list-date")}</HeaderCell>
-            <Cell>
-              {(rawData) => {
-                const snapshot = rawData as Snapshot;
-                return (
-                  <Whisper
-                    placement="top"
-                    trigger="hover"
-                    speaker={
-                      <Tooltip>
-                        {moment(snapshot.timestamp).format("lll")}
-                      </Tooltip>
-                    }
-                  >
-                    <div>{moment(snapshot.timestamp).format("YYYY-MM-DD")}</div>
-                  </Whisper>
-                );
-              }}
-            </Cell>
-          </Column>
+        <div>
+          <Table
+            data={snapshotList.snapshots}
+            loading={isLoading}
+            autoHeight={true}
+            id="table"
+          >
+            <Column flexGrow={10}>
+              <HeaderCell>{t("snapshot-list-date")}</HeaderCell>
+              <Cell>
+                {(rawData) => {
+                  const snapshot = rawData as Snapshot;
+                  return (
+                    <Whisper
+                      placement="top"
+                      trigger="hover"
+                      speaker={
+                        <Tooltip>
+                          {moment(snapshot.timestamp).format("lll")}
+                        </Tooltip>
+                      }
+                    >
+                      <div>
+                        {moment(snapshot.timestamp).format("YYYY-MM-DD")}
+                      </div>
+                    </Whisper>
+                  );
+                }}
+              </Cell>
+            </Column>
 
-          <Column flexGrow={10}>
-            <HeaderCell>{t("snapshot-list-source")}</HeaderCell>
-            <Cell>
-              {(rawData) => {
-                const snapshot = rawData as Snapshot;
-                return (
-                  <Tag>
-                    {snapshot.sourceKind == "Sync"
-                      ? t("snapshot-list-source-sync")
-                      : t("snapshot-list-source-upload")}
-                  </Tag>
-                );
-              }}
-            </Cell>
-          </Column>
+            <Column flexGrow={10}>
+              <HeaderCell>{t("snapshot-list-source")}</HeaderCell>
+              <Cell>
+                {(rawData) => {
+                  const snapshot = rawData as Snapshot;
+                  return (
+                    <Tag>
+                      {snapshot.sourceKind == "Sync"
+                        ? t("snapshot-list-source-sync")
+                        : t("snapshot-list-source-upload")}
+                    </Tag>
+                  );
+                }}
+              </Cell>
+            </Column>
 
-          <Column flexGrow={10} verticalAlign="middle">
-            <HeaderCell>
-              <MoreIcon />
-            </HeaderCell>
-            <Cell>
-              {(rawData) => {
-                const snapshot = rawData as Snapshot;
-                return (
-                  <div style={{ marginTop: "-3px" }}>
-                    <ButtonToolbar>
-                      <Button
-                        size="sm"
-                        onClick={() => {
-                          location.href =
-                            "/editor?viewing-snapshot=" + String(snapshot.id);
-                        }}
-                      >
-                        {t("snapshot-list-view")}
-                      </Button>
-                      <Button
-                        size="sm"
-                        onClick={async () => {
-                          const token = await Api.getSnapshotDownloadToken(
-                            snapshot.id
-                          );
-                          if (token.ok) {
-                            window.open(
-                              Api.backendUrl +
-                                "misc/download?token=" +
-                                token.ok,
-                              "_blank"
+            <Column flexGrow={10}>
+              <HeaderCell>
+                <MoreIcon />
+              </HeaderCell>
+              <Cell>
+                {(rawData) => {
+                  const snapshot = rawData as Snapshot;
+                  return (
+                    <div style={{ marginTop: "-3px" }}>
+                      <ButtonToolbar>
+                        <Button
+                          size="sm"
+                          onClick={() => {
+                            location.href =
+                              "/editor?viewing-snapshot=" + String(snapshot.id);
+                          }}
+                        >
+                          {t("snapshot-list-view")}
+                        </Button>
+                        <Button
+                          size="sm"
+                          onClick={async () => {
+                            const token = await Api.getSnapshotDownloadToken(
+                              snapshot.id
                             );
-                          } else {
-                            //TODO: error handling
-                          }
-                        }}
-                      >
-                        {t("snapshot-list-download")}
-                      </Button>
-                      <Button
-                        size="sm"
-                        onClick={() => openDeleteConfirmation(snapshot.id)}
-                      >
-                        {t("snapshot-list-delete")}
-                      </Button>
-                    </ButtonToolbar>
-                  </div>
-                );
-              }}
-            </Cell>
-          </Column>
-        </Table>
+                            if (token.ok) {
+                              window.open(
+                                Api.backendUrl +
+                                  "misc/download?token=" +
+                                  token.ok,
+                                "_blank"
+                              );
+                            } else {
+                              //TODO: error handling
+                            }
+                          }}
+                        >
+                          {t("snapshot-list-download")}
+                        </Button>
+                        <Button
+                          size="sm"
+                          onClick={() => openDeleteConfirmation(snapshot.id)}
+                        >
+                          {t("snapshot-list-delete")}
+                        </Button>
+                      </ButtonToolbar>
+                    </div>
+                  );
+                }}
+              </Cell>
+            </Column>
+          </Table>
+          <hr />
+          <Pagination
+            layout={["total", "-", "pager", "skip"]}
+            size={"xs"}
+            prev={true}
+            next={true}
+            first={true}
+            last={true}
+            limit={10}
+            total={snapshotList.numberOfSnapshots}
+            maxButtons={4}
+            activePage={snapshotListState.currentPage}
+            onChangePage={(page) => {
+              setSnapshotListState({
+                currentPage: page,
+              });
+            }}
+          />
+        </div>
       );
     }
   };
@@ -227,7 +268,6 @@ function DashboardSnapshot() {
             <span>{t("snapshot-list-title")}</span>
             <IconButton
               icon={<PlusIcon />}
-              appearance="ghost"
               onClick={() => {
                 setUploadDialogState({
                   uploadDate: null,

frontend/src/time-machine/Home.tsx
@@ -194,7 +194,7 @@ function Home() {
                     location.href = Api.backendUrl + "user/sso/github";
                   }}
                 >
-                  <GithubIcon style={{ fontSize: "2em" }} />{" "}
+                  <GithubIcon style={{ fontSize: "2em" }} />
                   {t("login-main-github")}
                 </Button>
               </div>

server/src/snapshot_handler.rs
@@ -24,33 +24,53 @@ struct SnapshotJson {
     pub source_kind: snapshot::SourceKind,
     pub note: Option<String>,
 }
-// TODO: pagination
-#[get("/")]
-async fn list_all(conn: Connection<'_, Db>, user: User) -> APIResponse {
+
+#[get("/?<page>&<page_size>")]
+async fn list_snapshots(
+    conn: Connection<'_, Db>,
+    user: User,
+    page: Option<usize>,
+    page_size: Option<usize>,
+) -> APIResponse {
     let db = conn.into_inner();
-    let snapshots = snapshot::Entity::find()
+
+    let page_size = page_size.unwrap_or(10);
+    if page_size > 200 {
+        return Ok((Status::BadRequest, json!({"error": "invalid_page_size"})));
+    }
+    let page = page.unwrap_or(1);
+
+    let snapshot_page = snapshot::Entity::find()
         .filter(snapshot::Column::UserId.eq(user.uid))
         .order_by_desc(snapshot::Column::Timestamp)
-        .all(db)
-        .await?;
-    let mut snapshot_list: Vec<SnapshotJson> = Vec::with_capacity(snapshots.len());
-    for snapshot in snapshots {
-        let snapshot::Model {
-            id,
-            user_id: _,
-            timestamp,
-            source_kind,
-            note,
-            sync_files: _,
-        } = snapshot;
-        snapshot_list.push(SnapshotJson {
-            id,
-            timestamp,
-            source_kind,
-            note,
+        .paginate(db, page_size);
+    // sea_orm page index start from 0
+    let snapshots = snapshot_page.fetch_page(page - 1).await?;
+    let counts = snapshot_page.num_items_and_pages().await?;
+    let snapshots: Vec<SnapshotJson> = snapshots
+        .into_iter()
+        .map(|snapshot| {
+            let snapshot::Model {
+                id,
+                user_id: _,
+                timestamp,
+                source_kind,
+                note,
+                sync_files: _,
+            } = snapshot;
+            SnapshotJson {
+                id,
+                timestamp,
+                source_kind,
+                note,
+            }
         })
-    }
-    Ok((Status::Ok, json!(snapshot_list)))
+        .collect();
+
+    Ok((
+        Status::Ok,
+        json!({"number_of_snapshots":counts.number_of_items,"number_of_pages":counts.number_of_pages,"snapshots":snapshots}),
+    ))
 }
 
 fn get_and_remove_uploaded_item(
@@ -280,7 +300,7 @@ async fn get_editor_view(
 
 pub fn routes() -> Vec<rocket::Route> {
     routes![
-        list_all,
+        list_snapshots,
         create,
         delete,
         get_download_token,

commit 5777d4fb2e22443c048e647a6ad7ecbd839e9eeb

Fix a bug that require user to login twice. (#85)
The bug was introduced by #71

frontend/src/time-machine/Api.ts
@@ -77,9 +77,9 @@ export default class Api {
     return token;
   }
 
-  public static readonly tokenHeaders = {
-    Authorization: "Bearer " + this.getToken(),
-  };
+  public static tokenHeaders() {
+    return { Authorization: "Bearer " + this.getToken() };
+  }
 
   private static clearToken() {
     sessionStorage.removeItem(this.tokenKey);
@@ -98,7 +98,7 @@ export default class Api {
   ): Promise<Result<any>> {
     try {
       console.log("requesting api:", method, " ", url);
-      const headers = needToken ? this.tokenHeaders : undefined;
+      const headers = needToken ? this.tokenHeaders() : undefined;
       const response = await axios({
         method,
         url: this.backendUrl + url,