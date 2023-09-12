d996af206b45de70a40d562c1d8cbfc581dfe554
chentian <lct8712@gmail.com>
2023-09-12 15:21:23 +0800

    clean code

--- editor/src/utils/GpxExport.ts
+++ editor/src/utils/GpxExport.ts
@@ -45,8 +45,6 @@ export function sortFilledList(grid: boolean[][]): number[][][] {
 }
 
 export function exportToGpx(lngLatList: number[][]): Blob {
-  // ${time}
-  // const time = file.meta.time ? `<time>${file.meta.time}</time>` : "";
   let time = new Date(GPX_START_TIME);
   const newGPX = `<?xml version="1.0" encoding="UTF-8" ?>
     <gpx version="1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/0" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd">
