755f1eda0b1b114795bbc47b5838ee57cfb06675
Klzy <972833608@qq.com>
2025-09-24 23:16:12 +0800

    Add OnDrive prompt (#119)

--- frontend/src/I18n.ts
+++ frontend/src/I18n.ts
@@ -39,6 +39,8 @@ i18n
           "data-source-title": "Data source",
           "data-share-link": "Share link",
           "data-share-link-help": "How to get share link",
+          "data-share-link-ondrive-not-working":
+            "OneDrive share link doesn't work right now",
           "data-sync-interval": "Sync interval",
           "data-share-link-every": "every",
           "data-upload-title": "Upload Data",
@@ -120,6 +122,7 @@ i18n
           "data-source-title": "数据源",
           "data-share-link": "共享链接",
           "data-share-link-help": "如何获取共享链接",
+          "data-share-link-ondrive-not-working": "OneDrive共享链接暂不可用",
           "data-sync-interval": "定时同步",
           "data-share-link-every": "每隔",
           "data-upload-title": "上传",
